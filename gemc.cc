721ddc15a4d6c2aeebfddc68c0e82b49df154b91
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2020-03-13 16:26:07 -0400

    just indentation

--- gemc.cc
+++ gemc.cc
@@ -18,27 +18,27 @@ int main(int argc, char* argv[])
 	// the option are loaded in utilities/defineOptions.cc
 	// they include the gemc core options and any frameworks options
 	GOptions *gopts = new GOptions(argc, argv, defineOptions());
-
+	
 	// print settings w/o defaults
 	gopts->printSettings(false);
-
-    // get gemc configuration options
-    auto gconf = goptions::getGConf(gopts);
-
-    // create QT app if gui is not zero
+	
+	// get gemc configuration options
+	auto gconf = goptions::getGConf(gopts);
+	
+	// create QT app if gui is not zero
 	createQtApplication(argc, argv, gconf.gui);
-
+	
 	// instantiating new User Interface Messenger
 	G4UImanager* UIM = G4UImanager::GetUIpointer();
-
+	
 	// cout destination for the UIM
 	UIM->SetCoutDestination(new G4UIsession);
-
-    
-    // init geant4 run manager with number of threads coming from options
-    G4MTRunManager *g4MTRunManager = new G4MTRunManager;
-    g4MTRunManager->SetNumberOfThreads(gconf.nthreads);
-
+	
+	
+	// init geant4 run manager with number of threads coming from options
+	G4MTRunManager *g4MTRunManager = new G4MTRunManager;
+	g4MTRunManager->SetNumberOfThreads(gconf.nthreads);
+	
 	return EXIT_SUCCESS;
 }
 
