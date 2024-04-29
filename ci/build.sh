116b34a228c7142bdd7292731ed82ce903cdcce7
mungaro <maurizio.ungaro@cnu.edu>
2024-04-29 12:06:48 -0400

    added assimp dependency and STL reader

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 # Purpose: compiles gemc and installs it in gemc
 
 # Container run:
-# docker run -it --rm jeffersonlab/gemc3:dev-fedora36 sh
+# docker run -it --rm jeffersonlab/gemc3:g3vdev-g4v11.2.1-almalinux93-local
 # git clone http://github.com/gemc/src /root/src && cd /root/src
 # git clone http://github.com/maureeungaro/src /root/src && cd /root/src
 # ./ci/build.sh
@@ -17,26 +17,19 @@ else
 fi
 
 function compileGEMC {
-	# getting number of available CPUS
-	copt=" -j"`getconf _NPROCESSORS_ONLN`" OPT=1"
-	echo
-	echo Compiling GEMC with options: "$copt"
-	scons SHOWENV=1 SHOWBUILD=1 $copt
-	# checking existence of executable
-	ls gemc
-	if [ $? -ne 0 ]; then
-		echo gemc executable not found
-		exit 1
-	fi
+	meson setup build --native-file=release.ini -Duse_root=true --wipe
+	cd build
+	meson configure -Dprefix=$GEMC
+	meson install
+	meson test
+	cd ..
 }
 
 compileGEMC
 echo
-echo "content of local directory:"
-ls -lrt
-echo
-echo "copying gemc to "$GEMC for scig and clas12-systems tests
-cp gemc $GEMC
-echo
-echo "content of "$GEMC":"
+echo "- Content of $GEMC"
 ls -lrt $GEMC
+echo "- Content of $GEMC/bin"
+ls -lrt $GEMC/bin
+echo "- Content of $GEMC/lib"
+ls -lrt $GEMC/lib
--- g4system/g4objectsFactories/cad/buildSolid.cc
+++ g4system/g4objectsFactories/cad/buildSolid.cc
@@ -8,54 +8,75 @@
 // - TessellatedMesh
 // https://en.cppreference.com/w/cpp/language/final
 // and why: https://devblogs.microsoft.com/oldnewthing/20200619-00/?p=103877
+
+// added USE_CADMESH_ASSIMP_READER as per Christophe README Optional Dependencies
+
+
+//// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
+//// !!! SET THIS BEFORE INCLUDING CADMESH.HH TO USE THE ASSIMP READER !!! //
+//// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
+#define USE_CADMESH_ASSIMP_READER
+//
+//// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
+//// !!! SET THIS IF YOU WANT ASSIMP TO BE THE DEFAULT READER !!! //
+//// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
+//#define CADMESH_DEFAULT_READER ASSIMP
+
 #include "CADMesh.hh"
 
-G4VSolid* G4CadSystemFactory::buildSolid(GOptions* gopt, GVolume *s, map<string, G4Volume*> *g4s)
-{
+G4VSolid *G4CadSystemFactory::buildSolid(GOptions *gopt, GVolume *s, map<string, G4Volume *> *g4s) {
 
-	string g4name = s->getG4Name();
-	bool verbosity = getVerbosity(gopt, g4name);
+    string g4name = s->getG4Name();
+    bool verbosity = getVerbosity(gopt, g4name);
 
-	// check dependencies first
-	if(!checkSolidDependencies(verbosity, s, g4s)) return nullptr;
+    // check dependencies first
+    if (!checkSolidDependencies(verbosity, s, g4s)) return nullptr;
 
-	// if the g4volume doesn't exist, create one and add it to the map
-	G4Volume *thisG4Volume = nullptr;
+    // if the g4volume doesn't exist, create one and add it to the map
+    G4Volume *thisG4Volume = nullptr;
 
-	// check if g4s already exists
-	if(g4s->find(g4name) != g4s->end()) {
-		thisG4Volume = (*g4s)[g4name];
-		// if the solid is already built, returning it
-		if (thisG4Volume->getSolid() != nullptr) return thisG4Volume->getSolid();
-	} else {
-		thisG4Volume = new G4Volume();
-		// adding volume to the map
-		(*g4s)[g4name] = thisG4Volume;
-	}
+    // check if g4s already exists
+    if (g4s->find(g4name) != g4s->end()) {
+        thisG4Volume = (*g4s)[g4name];
+        // if the solid is already built, returning it
+        if (thisG4Volume->getSolid() != nullptr) return thisG4Volume->getSolid();
+    } else {
+        thisG4Volume = new G4Volume();
+        // adding volume to the map
+        (*g4s)[g4name] = thisG4Volume;
+    }
 
-	string fileName  = s->getDescription();
-	G4String g4filename = fileName;
-	
-	auto extension = getStringVectorFromStringWithDelimiter(fileName, ".").back();
+    string fileName = s->getDescription();
+    G4String g4filename = fileName;
+
+    auto extension = getStringVectorFromStringWithDelimiter(fileName, ".").back();
 
 
-	if ( extension == "ply" ) {
-		auto mesh = CADMesh::TessellatedMesh::FromPLY(g4filename);
-		mesh->SetScale(CLHEP::mm);
-		mesh->SetReverse(false);
+    if (extension == "ply") {
+        // auto mesh = CADMesh::TessellatedMesh::FromPLY(g4filename);
 
-		thisG4Volume->setSolid(mesh->GetSolid(), verbosity);
-		return thisG4Volume->getSolid();
+        auto mesh = CADMesh::TessellatedMesh::From(g4filename, CADMesh::File::ASSIMP());
 
-	} else if ( extension == "stl" ) {
-		auto mesh = CADMesh::TessellatedMesh::FromSTL(g4filename);
-		mesh->SetScale(CLHEP::mm);
-		mesh->SetReverse(false);
-		thisG4Volume->setSolid(mesh->GetSolid(), verbosity);
-		return thisG4Volume->getSolid();
-	}
 
-	
-	return nullptr;
+        mesh->SetScale(CLHEP::mm);
+        mesh->SetReverse(false);
+
+        thisG4Volume->setSolid(mesh->GetSolid(), verbosity);
+        return thisG4Volume->getSolid();
+
+    } else if (extension == "stl") {
+
+    // auto mesh = CADMesh::TessellatedMesh::FromSTL(g4filename);
+
+        auto mesh = CADMesh::TessellatedMesh::From(g4filename, CADMesh::File::ASSIMP());
+
+        mesh->SetScale(CLHEP::mm);
+        mesh->SetReverse(false);
+        thisG4Volume->setSolid(mesh->GetSolid(), verbosity);
+        return thisG4Volume->getSolid();
+    }
+
+
+    return nullptr;
 
 }
