5d46337f75f6b2d69cce9ed942c86145f5167a52
mungaro <maurizio.ungaro@cnu.edu>
2024-07-24 12:29:20 -0400

    more debugging, solution 9

--- ci/build.sh
+++ ci/build.sh
@@ -19,6 +19,7 @@ fi
 function compileGEMC {
   echo " > Current dir: $(pwd)"
   ls -l
+  git config --global --add safe.directory $PWD
   git describe --tags --abbrev=0
   echo " > After git describe"
 	meson setup build --native-file=release.ini -Duse_root=true --wipe
