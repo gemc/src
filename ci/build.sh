b55d7a638836030c7afb38b56cf3f24b7d768b4c
mungaro <maurizio.ungaro@cnu.edu>
2024-08-06 11:38:28 -0400

    testing max n jobs and pgo on ubuntu

--- ci/build.sh
+++ ci/build.sh
@@ -13,14 +13,23 @@
 
 source ci/functions.sh
 sanitize_option="-Db_sanitize=$1"
+pgo=""
 
 # reset sanitize option if on mac
 if [[ "$OSTYPE" == "darwin"* ]] || [[ "standard" == $1 ]]; then
     sanitize_option=""
 fi
 
+# if on ubuntu, use pgo generate
+if [ -f /etc/os-release ]; then
+    if grep -q "Ubuntu" /etc/os-release; then
+        pgo="-Dpgo=generate"
+    fi
+fi
+
+
 echo " > Running build Configure"
-meson setup build --native-file=release.ini -Duse_root=true $sanitize_option -Dprefix=$GEMC --wipe
+meson setup build --native-file=release.ini -Duse_root=true $sanitize_option $pgo -Dprefix=$GEMC --wipe
 cd build
 echo " > Running meson compile and install"
 meson compile -v
