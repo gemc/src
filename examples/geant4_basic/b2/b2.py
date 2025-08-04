978441715bcb60e9a3b8600ec4901dcee0e9fa36
Maurizio Ungaro <maureeungaro@gmail.com>
2025-08-04 15:20:25 -0400

    using installed gemc for the examples

--- examples/geant4_basic/b2/b2.py
+++ examples/geant4_basic/b2/b2.py
@@ -5,12 +5,12 @@ from utils_api import GConfiguration
 
 # B2
 from geometry import build_geometry
-#from materials import define_materials
+from materials import define_materials
 
 def main():
 	configuration = GConfiguration("examples", "b2")
 	build_geometry(configuration)
-#	define_materials(configuration)
+	define_materials(configuration)
 	configuration.printC()
 
 if __name__ == "__main__":
