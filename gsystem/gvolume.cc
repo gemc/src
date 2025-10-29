12b0c2942d2f14c7db862ab760efe6a8313e4a5e
Maurizio Ungaro <maureeungaro@gmail.com>
2025-10-29 15:10:13 -0400

    added opacity

--- gsystem/gvolume.cc
+++ gsystem/gvolume.cc
@@ -9,127 +9,141 @@ using namespace gutilities;
 // need to set pCopyNo with unique identifier
 // see c++ thread safe ID generation function
 GVolume::GVolume(const std::shared_ptr<GLogger>& logger,
-                 const std::string&              s, vector<string> pars,
-                 const std::string&              importPath) :
-	GBase(logger),
-	system(s),
-	importFilename(importPath) {
-	if (pars.size() != GVOLUMENUMBEROFPARS) {
-		for (auto& parameter : pars) { log->warning(" - parameter ", parameter); }
+                 const std::string& s, vector<string> pars,
+                 const std::string& importPath) :
+    GBase(logger),
+    system(s),
+    importFilename(importPath)
+{
+    if (pars.size() != GVOLUMENUMBEROFPARS)
+    {
+        for (auto& parameter : pars) { log->warning(" - parameter ", parameter); }
 
-		log->error(ERR_GWRONGNUMBEROFPARS,
-		           "Incorrect number of system parameters for GVolume: ", pars.size(), ", it should be ",
-		           GVOLUMENUMBEROFPARS);
-	}
-	else {
-		// size is already checked in addVolume, the only interface to volume
-		int i = 0;
+        log->error(ERR_GWRONGNUMBEROFPARS,
+                   "Incorrect number of system parameters for GVolume: ", pars.size(), ", it should be ",
+                   GVOLUMENUMBEROFPARS);
+    }
+    else
+    {
+        // size is already checked in addVolume, the only interface to volume
+        int i = 0;
 
-		name = removeAllSpacesFromString(pars[i++]);
+        name = removeAllSpacesFromString(pars[i++]);
 
-		// checking that name does not contain GSYSTEM_DELIMITER
-		if (name.find(GSYSTEM_DELIMITER) != string::npos) {
-			log->error(ERR_GVOLUMENAMECONTAINSINVALID,
-			           "the gVolume name <", name, "> contains the invalid character: <", GSYSTEM_DELIMITER,
-			           ">. Exiting.");
-		}
+        // checking that name does not contain GSYSTEM_DELIMITER
+        if (name.find(GSYSTEM_DELIMITER) != string::npos)
+        {
+            log->error(ERR_GVOLUMENAMECONTAINSINVALID,
+                       "the gVolume name <", name, "> contains the invalid character: <", GSYSTEM_DELIMITER,
+                       ">. Exiting.");
+        }
 
-		type           = removeAllSpacesFromString(pars[i++]);
-		parameters     = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		material       = removeAllSpacesFromString(pars[i++]);
-		motherName     = removeAllSpacesFromString(pars[i++]);
-		pos            = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		rot            = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		emfield        = removeAllSpacesFromString(pars[i++]);
-		string pvis    = removeAllSpacesFromString(pars[i++]);
-		visible        = (pvis == "1") ? true : false;
-		style          = stoi(removeAllSpacesFromString(pars[i++]));
-		color          = removeAllSpacesFromString(pars[i++]);
-		digitization   = removeAllSpacesFromString(pars[i++]);
-		gidentity      = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		copyOf         = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		solidsOpr      = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		mirror         = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		string pexists = removeAllSpacesFromString(pars[i++]);
-		exist          = (pexists == "1") ? true : false;
+        type = removeAllSpacesFromString(pars[i++]);
+        parameters = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        material = removeAllSpacesFromString(pars[i++]);
+        motherName = removeAllSpacesFromString(pars[i++]);
+        pos = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        rot = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        emfield = removeAllSpacesFromString(pars[i++]);
+        string pvis = removeAllSpacesFromString(pars[i++]);
+        visible = (pvis == "1") ? true : false;
+        style = stoi(removeAllSpacesFromString(pars[i++]));
+        color = removeAllSpacesFromString(pars[i++]);
+        opacity = stod(removeAllSpacesFromString(pars[i++]));
+        digitization = removeAllSpacesFromString(pars[i++]);
+        gidentity = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        copyOf = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        solidsOpr = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        mirror = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        string pexists = removeAllSpacesFromString(pars[i++]);
+        exist = (pexists == "1") ? true : false;
 
-		// these will be assigned later
-		g4name       = UNINITIALIZEDSTRINGQUANTITY;
-		g4motherName = UNINITIALIZEDSTRINGQUANTITY;
+        // these will be assigned later
+        g4name = UNINITIALIZEDSTRINGQUANTITY;
+        g4motherName = UNINITIALIZEDSTRINGQUANTITY;
 
-		description = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		variation   = removeLeadingAndTrailingSpacesFromString(pars[i++]);
-		runno       = stoi(removeAllSpacesFromString(pars[i++]));
+        description = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        variation = removeLeadingAndTrailingSpacesFromString(pars[i++]);
+        runno = stoi(removeAllSpacesFromString(pars[i++]));
 
-		// modifiers - accessed through options/jcard
-		shift = GSYSTEMNOMODIFIER;
-		tilt  = GSYSTEMNOMODIFIER;
-	}
+        // modifiers - accessed through options/jcard
+        shift = GSYSTEMNOMODIFIER;
+        tilt = GSYSTEMNOMODIFIER;
+    }
 }
 
 
-std::ostream& operator<<(std::ostream& stream, const GVolume& gVol) {
-	string style = "unknown";
-	if (gVol.style == 0) { style = "wireframe"; }
-	else if (gVol.style == 1) { style = "solid"; }
-	string visibility = "yes";
-	if (!gVol.visible) { visibility = "no"; }
+std::ostream& operator<<(std::ostream& stream, const GVolume& gVol)
+{
+    string style = "unknown";
+    if (gVol.style == 0) { style = "wireframe"; }
+    else if (gVol.style == 1) { style = "solid"; }
+    string visibility = "yes";
+    if (!gVol.visible) { visibility = "no"; }
 
-	stream << std::endl;
-	stream << "   - Name:            " << gVol.name << "  -  " << gVol.description << std::endl;
-	stream << "   - System:         " << gVol.system << std::endl;
-	stream << "   - Variation:       " << gVol.variation << std::endl;
-	stream << "   - Run Number:      " << gVol.runno << std::endl;
-	if (gVol.copyOf != "" && gVol.copyOf != UNINITIALIZEDSTRINGQUANTITY) stream << "   - copyOf:          " << gVol.copyOf << std::endl;
-	if (gVol.solidsOpr != "" && gVol.solidsOpr != UNINITIALIZEDSTRINGQUANTITY) stream << "   - solidsOpr:            " << gVol.solidsOpr << std::endl;
-	if (gVol.type != "" && gVol.type != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Type:            " << gVol.type << std::endl;
-	if (gVol.parameters != "" && gVol.parameters != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Parameters:      " << gVol.parameters << std::endl;
-	stream << "   - Material:        " << gVol.material << std::endl;
-	stream << "   - Mother:          " << gVol.motherName << std::endl;
-	stream << "   - Positions:       " << gVol.pos << std::endl;
-	stream << "   - Rotation(s):     " << gVol.rot << std::endl;
-	if (gVol.emfield != "" && gVol.emfield != UNINITIALIZEDSTRINGQUANTITY) stream << "   - E.M. emfield:      " << gVol.emfield << std::endl;
-	if (gVol.digitization != "" && gVol.digitization != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Digitization:    " << gVol.digitization << std::endl;
-	if (gVol.gidentity != "" && gVol.gidentity != UNINITIALIZEDSTRINGQUANTITY) stream << "   - GIdentity:       " << gVol.gidentity << std::endl;
-	stream << "   - Col, Vis, Style: " << gVol.color << ", " << visibility << ", " << style << std::endl;
-	stream << std::endl;
+    stream << std::endl;
+    stream << "   - Name:            " << gVol.name << "  -  " << gVol.description << std::endl;
+    stream << "   - System:         " << gVol.system << std::endl;
+    stream << "   - Variation:       " << gVol.variation << std::endl;
+    stream << "   - Run Number:      " << gVol.runno << std::endl;
+    if (gVol.copyOf != "" && gVol.copyOf != UNINITIALIZEDSTRINGQUANTITY) stream << "   - copyOf:          " << gVol.
+        copyOf << std::endl;
+    if (gVol.solidsOpr != "" && gVol.solidsOpr != UNINITIALIZEDSTRINGQUANTITY) stream << "   - solidsOpr:            "
+        << gVol.solidsOpr << std::endl;
+    if (gVol.type != "" && gVol.type != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Type:            " << gVol.type <<
+        std::endl;
+    if (gVol.parameters != "" && gVol.parameters != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Parameters:      " <<
+        gVol.parameters << std::endl;
+    stream << "   - Material:        " << gVol.material << std::endl;
+    stream << "   - Mother:          " << gVol.motherName << std::endl;
+    stream << "   - Positions:       " << gVol.pos << std::endl;
+    stream << "   - Rotation(s):     " << gVol.rot << std::endl;
+    if (gVol.emfield != "" && gVol.emfield != UNINITIALIZEDSTRINGQUANTITY) stream << "   - E.M. emfield:      " << gVol.
+        emfield << std::endl;
+    if (gVol.digitization != "" && gVol.digitization != UNINITIALIZEDSTRINGQUANTITY) stream << "   - Digitization:    "
+        << gVol.digitization << std::endl;
+    if (gVol.gidentity != "" && gVol.gidentity != UNINITIALIZEDSTRINGQUANTITY) stream << "   - GIdentity:       " <<
+        gVol.gidentity << std::endl;
+    stream << "   - Col, Vis, Style: " << gVol.color << ", " << visibility << ", " << style << std::endl;
+    stream << std::endl;
 
-	return stream;
+    return stream;
 }
 
 
-GVolume::GVolume(const string& rootVolumeDefinition, const std::shared_ptr<GLogger>& logger) : GBase(logger) {
-	vector<string> rootDefinitions = getStringVectorFromStringWithDelimiter(rootVolumeDefinition, " ");
-	string         volumeParameters;
+GVolume::GVolume(const string& rootVolumeDefinition, const std::shared_ptr<GLogger>& logger) : GBase(logger)
+{
+    vector<string> rootDefinitions = getStringVectorFromStringWithDelimiter(rootVolumeDefinition, " ");
+    string volumeParameters;
 
-	for (size_t i = 1; i < rootDefinitions.size() - 1; i++) { volumeParameters += ", " + rootDefinitions[i]; }
+    for (size_t i = 1; i < rootDefinitions.size() - 1; i++) { volumeParameters += ", " + rootDefinitions[i]; }
 
-	name         = ROOTWORLDGVOLUMENAME;
-	system       = ROOTWORLDGVOLUMENAME;
-	variation    = "default";
-	type         = rootDefinitions[0];
-	parameters   = volumeParameters;
-	material     = rootDefinitions.back();
-	motherName   = MOTHEROFUSALL;
-	pos          = DEFAULTPOSITION;
-	rot          = DEFAULTROTATION;
-	emfield      = "";
-	visible      = false;
-	style        = 0; // wireframe
-	color        = "ccffff";
-	digitization = "";
-	gidentity    = "";
-	copyOf       = "";
-	solidsOpr    = "";
-	mirror       = "";
-	exist        = true;
+    name = ROOTWORLDGVOLUMENAME;
+    system = ROOTWORLDGVOLUMENAME;
+    variation = "default";
+    type = rootDefinitions[0];
+    parameters = volumeParameters;
+    material = rootDefinitions.back();
+    motherName = MOTHEROFUSALL;
+    pos = DEFAULTPOSITION;
+    rot = DEFAULTROTATION;
+    emfield = "";
+    visible = false;
+    style = 0; // wireframe
+    color = "ccffff";
+    digitization = "";
+    gidentity = "";
+    copyOf = "";
+    solidsOpr = "";
+    mirror = "";
+    exist = true;
 
-	description = "root volume";
+    description = "root volume";
 
-	// modifiers - accessed through options/jcard
-	shift = GSYSTEMNOMODIFIER;
-	tilt  = GSYSTEMNOMODIFIER;
+    // modifiers - accessed through options/jcard
+    shift = GSYSTEMNOMODIFIER;
+    tilt = GSYSTEMNOMODIFIER;
 
-	// set file with its path if it's a CAD/GDML import
-	importFilename = "none";
+    // set file with its path if it's a CAD/GDML import
+    importFilename = "none";
 }
--- gsystem/gvolume.h
+++ gsystem/gvolume.h
@@ -9,147 +9,155 @@
 #include <utility>
 
 
-class GVolume : public GBase<GVolume> {
+class GVolume : public GBase<GVolume>
+{
 public:
-	GVolume(const std::shared_ptr<GLogger>& log, const std::string& system, std::vector<std::string> pars,  const std::string& importPath = UNINITIALIZEDSTRINGQUANTITY);
+    GVolume(const std::shared_ptr<GLogger>& log, const std::string& system, std::vector<std::string> pars,
+            const std::string& importPath = UNINITIALIZEDSTRINGQUANTITY);
 
-	explicit GVolume(const std::string& rootVolumeDefinition, const std::shared_ptr<GLogger>& log); // special constructor for root volume
+    explicit GVolume(const std::string& rootVolumeDefinition, const std::shared_ptr<GLogger>& log);
+    // special constructor for root volume
 
-	// Define a virtual clone method to be used in the copy constructor
-	[[nodiscard]] virtual std::unique_ptr<GVolume> clone() const {
-		return std::make_unique<GVolume>(*this); // Make a copy of the current object
-	}
+    // Define a virtual clone method to be used in the copy constructor
+    [[nodiscard]] virtual std::unique_ptr<GVolume> clone() const
+    {
+        return std::make_unique<GVolume>(*this); // Make a copy of the current object
+    }
 
-	// Virtual destructor, needed for when unique_ptr<GVolume> is deleted
-	virtual ~GVolume() = default;
+    // Virtual destructor, needed for when unique_ptr<GVolume> is deleted
+    virtual ~GVolume() = default;
 
 private:
-	std::string system;         // System of provenience
-	std::string name;           // Name of the volume
-	std::string motherName;     // Mother Volume name
-	std::string description;    // Volume Description, for documentation
-	std::string importFilename; // For imports, filename with the path, set with the import factory
+    std::string system; // System of provenience
+    std::string name; // Name of the volume
+    std::string motherName; // Mother Volume name
+    std::string description; // Volume Description, for documentation
+    std::string importFilename; // For imports, filename with the path, set with the import factory
 
-	// solid parameters
-	std::string type;       // solid type. This follows the GEANT4 definitions
-	std::string parameters; // vector of parameters used in the geant4 solid constructor
+    // solid parameters
+    std::string type; // solid type. This follows the GEANT4 definitions
+    std::string parameters; // vector of parameters used in the geant4 solid constructor
 
-	// solid visualization style
-	bool        visible; // visibility of the detector: 0=invisible 1=visible
-	int         style;   // Visual style: 0=wireframe 1=solid
-	std::string color;   // 6(7) digits colors in RRGGBB format. The last optional digit is transparency
+    // solid visualization style
+    bool visible; // visibility of the detector: 0=invisible 1=visible
+    int style; // Visual style: 0=wireframe 1=solid
+    std::string color; // 6(7) digits colors in RRGGBB format. The last optional digit is transparency
+    double opacity;
 
-	// logical attributes
-	std::string material; // Volume Material name.
-	std::string emfield;  // Associated Magnetic Field name.
+    // logical attributes
+    std::string material; // Volume Material name.
+    std::string emfield; // Associated Magnetic Field name.
 
-	// physical attributes
-	std::string pos;   // Defines the position relative to the mother volume
-	std::string rot;   // Define the rotation Matrix, defined by rotations along x,y,z axis relative to the mother volume
-	std::string shift; // Position modifier
-	std::string tilt;  // Rotation modifier
-	bool        exist; // Existence modifier
+    // physical attributes
+    std::string pos; // Defines the position relative to the mother volume
+    std::string rot; // Define the rotation Matrix, defined by rotations along x,y,z axis relative to the mother volume
+    std::string shift; // Position modifier
+    std::string tilt; // Rotation modifier
+    bool exist; // Existence modifier
 
-	std::string digitization; // Assigns digitization type and collection ID
-	std::string gidentity;    // String with identifiers in it. Example: 'sector: 2, layer: 4, wire; 33'
+    std::string digitization; // Assigns digitization type and collection ID
+    std::string gidentity; // String with identifiers in it. Example: 'sector: 2, layer: 4, wire; 33'
 
-	// special cases
-	std::string copyOf;    // name of gvolume to copy from
-	std::string solidsOpr; // solid boolean operation
+    // special cases
+    std::string copyOf; // name of gvolume to copy from
+    std::string solidsOpr; // solid boolean operation
 
-	int pCopyNo{}; // should be set to 0 for the first volume of a given type
+    int pCopyNo{}; // should be set to 0 for the first volume of a given type
 
-	// mirrors
-	std::string mirror;
+    // mirrors
+    std::string mirror;
 
-	// the map key names used in geant4 contain the system name
-	// these are assigned by gworld after all voumes are loaded
-	std::string g4name;       // Name of the g4volume
-	std::string g4motherName; // Name of the g4 Mother volume
+    // the map key names used in geant4 contain the system name
+    // these are assigned by gworld after all voumes are loaded
+    std::string g4name; // Name of the g4volume
+    std::string g4motherName; // Name of the g4 Mother volume
 
-	// variation and run number for this gvolume
-	std::string variation;
-	int         runno{};
+    // variation and run number for this gvolume
+    std::string variation;
+    int runno{};
 
-	friend std::ostream& operator<<(std::ostream& stream, const GVolume&); // Logs infos on screen.
+    friend std::ostream& operator<<(std::ostream& stream, const GVolume&); // Logs infos on screen.
 
 public:
-	[[nodiscard]] std::string getSystem() const { return system; }
+    [[nodiscard]] std::string getSystem() const { return system; }
 
-	[[nodiscard]] std::string getName() const { return name; }
+    [[nodiscard]] std::string getName() const { return name; }
 
-	[[nodiscard]] std::string getMotherName() const { return motherName; }
+    [[nodiscard]] std::string getMotherName() const { return motherName; }
 
-	[[nodiscard]] std::string getG4Name() const { return g4name; }
+    [[nodiscard]] std::string getG4Name() const { return g4name; }
 
-	[[nodiscard]] std::string getG4MotherName() const { return g4motherName; }
+    [[nodiscard]] std::string getG4MotherName() const { return g4motherName; }
 
-	[[nodiscard]] std::vector<double> getDetectorDimensions() const {
-		if (parameters == UNINITIALIZEDSTRINGQUANTITY) { return {0}; }
-		else { return gutilities::getG4NumbersFromString(parameters); }
-	}
+    [[nodiscard]] std::vector<double> getDetectorDimensions() const
+    {
+        if (parameters == UNINITIALIZEDSTRINGQUANTITY) { return {0}; }
+        else { return gutilities::getG4NumbersFromString(parameters); }
+    }
 
-	[[nodiscard]] std::string getType() const { return type; }
+    [[nodiscard]] std::string getType() const { return type; }
 
-	[[nodiscard]] std::string getParameters() const { return parameters; }
+    [[nodiscard]] std::string getParameters() const { return parameters; }
 
-	[[nodiscard]] std::string getMaterial() const { return material; }
+    [[nodiscard]] std::string getMaterial() const { return material; }
 
-	[[nodiscard]] int getPCopyNo() const { return pCopyNo; }
+    [[nodiscard]] int getPCopyNo() const { return pCopyNo; }
 
-	[[nodiscard]] std::string_view getColor() const { return color; }
+    [[nodiscard]] std::string_view getColor() const { return color; }
 
-	[[nodiscard]] std::string getEMField() const { return emfield; }
+    [[nodiscard]] double getOpacity() const { return opacity; }
 
-	[[nodiscard]] bool isVisible() const { return visible; }
+    [[nodiscard]] std::string getEMField() const { return emfield; }
 
-	[[nodiscard]] int getStyle() const { return style; }
+    [[nodiscard]] bool isVisible() const { return visible; }
 
-	[[nodiscard]] bool getExistence() const { return exist; }
+    [[nodiscard]] int getStyle() const { return style; }
 
-	[[nodiscard]] std::string getPos() const { return pos; }
+    [[nodiscard]] bool getExistence() const { return exist; }
 
-	[[nodiscard]] std::string getRot() const { return rot; }
+    [[nodiscard]] std::string getPos() const { return pos; }
 
-	[[nodiscard]] std::string getShift() const { return shift; }
+    [[nodiscard]] std::string getRot() const { return rot; }
 
-	[[nodiscard]] std::string getTilt() const { return tilt; }
+    [[nodiscard]] std::string getShift() const { return shift; }
 
-	[[nodiscard]] std::string getDigitization() const { return digitization; }
+    [[nodiscard]] std::string getTilt() const { return tilt; }
 
-	[[nodiscard]] std::string getGIdentity() const { return gidentity; }
+    [[nodiscard]] std::string getDigitization() const { return digitization; }
 
-	// special cases
-	[[nodiscard]] std::string getCopyOf() const { return copyOf; }
+    [[nodiscard]] std::string getGIdentity() const { return gidentity; }
 
-	[[nodiscard]] std::string getSolidsOpr() const { return solidsOpr; }
+    // special cases
+    [[nodiscard]] std::string getCopyOf() const { return copyOf; }
 
-	[[nodiscard]] std::string getDescription() const { return description; }
+    [[nodiscard]] std::string getSolidsOpr() const { return solidsOpr; }
 
-	// assign modifiers
-	void applyShift(std::string s) { shift = std::move(s); }
+    [[nodiscard]] std::string getDescription() const { return description; }
 
-	void applyTilt(std::string t) { tilt = std::move(t); }
+    // assign modifiers
+    void applyShift(std::string s) { shift = std::move(s); }
 
-	void modifyExistence(bool e) { exist = e; }
+    void applyTilt(std::string t) { tilt = std::move(t); }
 
-	void resetMotherName(std::string m) { motherName = std::move(m); }
+    void modifyExistence(bool e) { exist = e; }
 
-	void setColor(std::string c) { color = std::move(c); }
+    void resetMotherName(std::string m) { motherName = std::move(m); }
 
-	void setMaterial(std::string m) { material = std::move(m); }
+    void setColor(std::string c) { color = std::move(c); }
 
-	void setDigitization(std::string d) { digitization = std::move(d); }
+    void setMaterial(std::string m) { material = std::move(m); }
 
-	void setGIdentity(std::string g) { gidentity = std::move(g); }
+    void setDigitization(std::string d) { digitization = std::move(d); }
 
-	// imported volumes
-	std::string getImportedFile() { return importFilename; }
+    void setGIdentity(std::string g) { gidentity = std::move(g); }
 
-	// assign g4names
-	inline void assignG4Names(const std::string& g4n, const std::string& g4m) {
-		g4name       = g4n;
-		g4motherName = g4m;
-	}
+    // imported volumes
+    std::string getImportedFile() { return importFilename; }
 
+    // assign g4names
+    inline void assignG4Names(const std::string& g4n, const std::string& g4m)
+    {
+        g4name = g4n;
+        g4motherName = g4m;
+    }
 };
