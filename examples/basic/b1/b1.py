b2d501bd662c84ff25b8c47aa25000e8d1c2fb7a
Maurizio Ungaro <maureeungaro@gmail.com>
2026-04-21 10:18:29 -0400

    moved geometry b1 into b1.py

--- examples/basic/b1/b1.py
+++ examples/basic/b1/b1.py
@@ -1,11 +1,56 @@
 #!/usr/bin/env python3
-
-# gemc api
 from gconfiguration import autogeometry
-
-# build_geometry is in geometry.py
-from geometry import build_geometry
+from gvolume import GVolume
 
 cfg = autogeometry("examples", "b1")
-build_geometry(cfg)
 
+gvolume = GVolume('root')
+gvolume.description = 'World'
+gvolume.make_box(1.2*100.0, 1.2*100.0, 1.2*150.0)
+gvolume.material = 'G4_AIR'
+gvolume.color = '889922'
+gvolume.style = 0
+gvolume.publish(cfg)
+
+gvolume = GVolume('Envelope')
+gvolume.description = 'B1 Envelope'
+gvolume.make_box(100.0, 100.0, 150.0)
+gvolume.material = 'G4_WATER'
+gvolume.color = 'turquoise'
+gvolume.style = 0
+gvolume.publish(cfg)
+
+shape1_rmina = 0.0
+shape1_rmaxa = 20.0
+shape1_rminb = 0.0
+shape1_rmaxb = 40.0
+shape1_hz = 30.0
+shape1_phimin = 0.0
+shape1_phimax = 360.0
+
+gvolume = GVolume('Shape1')
+gvolume.mother = 'Envelope'
+gvolume.description = 'B1 Shape1'
+gvolume.make_cons(shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz, shape1_phimin, shape1_phimax)
+gvolume.material = 'G4_A-150_TISSUE'
+gvolume.color = 'pink'
+gvolume.set_position(0, 20, -70)
+gvolume.publish(cfg)
+
+shape2_dxa = 120
+shape2_dxb = 120
+shape2_dya = 100
+shape2_dyb = 160
+shape2_dz = 60
+
+gvolume = GVolume('Shape2')
+gvolume.mother = 'Envelope'
+gvolume.description = 'B1 Shape2'
+gvolume.make_trapezoid(shape2_dxa * 0.5, shape2_dxb * 0.5, shape2_dya * 0.5, shape2_dyb * 0.5, shape2_dz * 0.5)
+gvolume.material = 'G4_BONE_COMPACT_ICRU'
+gvolume.color = 'lightgray'
+gvolume.set_position(0, -10, 70)
+gvolume.digitization = 'dosimeter'     # collects edep, dose
+gvolume.set_identifier('mydosimeter', 1)
+gvolume.opacity = 0.1
+gvolume.publish(cfg)
