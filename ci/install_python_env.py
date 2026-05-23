50429da6ce76d23258655cabc07869250061b3e7
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2026-05-23 16:44:38 -0400

    pygemc installed on meson install

---
+++ ci/install_python_env.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Install the pygemc Python environment into the install prefix.
+
+Called by meson.add_install_script() during 'meson install'.
+Creates ${prefix}/python_env/ with pygemc and all its dependencies so that
+users only need to add ${prefix}/python_env/bin to PATH.
+
+Environment variables set by meson:
+  MESON_INSTALL_PREFIX  -- effective install prefix (respects DESTDIR)
+
+Arguments:
+  sys.argv[1]  -- absolute path to the pygemc source directory
+"""
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+prefix    = Path(os.environ['MESON_INSTALL_PREFIX'])
+pygemc_src = sys.argv[1]
+venv_dir  = prefix / 'python_env'
+
+print(f' > Installing Python environment at {venv_dir}')
+subprocess.run([sys.executable, '-m', 'venv', str(venv_dir)], check=True)
+
+venv_python = str(venv_dir / 'bin' / 'python3')
+# Non-editable install so the venv is self-contained after installation.
+subprocess.run([venv_python, '-m', 'pip', 'install', pygemc_src], check=True)
